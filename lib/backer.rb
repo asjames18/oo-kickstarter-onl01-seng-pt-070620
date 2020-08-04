49d871ffcbbd44b15d87bc4fabc0fae1f1aa0e01
Antonio James <asjames18@gmail.com>
2020-08-04 02:00:24 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,11 +2,12 @@ class Backer
 
   attr_accessor :backed_projects
 
-attr_reader :name
+attr_reader :name, :project
 
 def initialize (name)
   @name = "Avi"
   @backed_projects = []
+  @project = "Project With So Much Amaze"
 end
 
 end
